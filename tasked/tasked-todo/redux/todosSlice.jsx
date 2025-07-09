eff8313c01a21e7830070e1ad5a46fc134d2654f
Hirushi Rathnayake <hirushir99@gmail.com>
2025-07-09 20:53:53 +0530

    create todoslice for state management

---
+++ tasked/tasked-todo/redux/todosSlice.jsx
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const loadInitialTodos = async () => {
+  const data = await AsyncStorage.getItem('TODOS');
+  return data ? JSON.parse(data) : [];
+};
+
+const saveTodos = async (todos) => {
+  await AsyncStorage.setItem('TODOS', JSON.stringify(todos));
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    setTodos(state, action) {
+      return action.payload;
+    },
+    addTodo(state, action) {
+      state.push(action.payload);
+      saveTodos(state);
+    },
+    toggleTodo(state, action) {
+      const todo = state.find(t => t.id === action.payload);
+      if (todo) todo.completed = !todo.completed;
+      saveTodos(state);
+    },
+    editTodo(state, action) {
+      const { id, title } = action.payload;
+      const todo = state.find(t => t.id === id);
+      if (todo) todo.title = title;
+      saveTodos(state);
+    },
+    deleteTodo(state, action) {
+      const newState = state.filter(t => t.id !== action.payload);
+      saveTodos(newState);
+      return newState;
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, editTodo, deleteTodo, setTodos } = todosSlice.actions;
+export default todosSlice.reducer;
+export { loadInitialTodos };
